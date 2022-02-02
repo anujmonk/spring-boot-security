856cdf539b623a5e4219cda0a4c00dbac3676170
anujsharma <sharma.anuj15.github@gmail.com>
2022-02-02 09:56:59 -0500

    Configured Authorization for user and admin role

--- src/main/java/com/springboot/security/springbootsecurity/SecurityConfiguration.java
+++ src/main/java/com/springboot/security/springbootsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.springboot.security.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -21,7 +22,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         .withUser("foo")
         .password("foo")
         .roles("ADMIN");
-        
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+            .antMatchers("/admin").hasRole("ADMIN")
+            .antMatchers("/user").hasAnyRole("USER","ADMIN")
+            .antMatchers("/").permitAll()
+            .and().formLogin();
+
     }
 
     @Bean
